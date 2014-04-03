11ce6fb4ce6f887dcfc5a39a6452b09e485b153f
kyeah <kevinyeah@utexas.edu>
2014-04-03 15:35:20 -0500

    import cegui, transparent material

--- a4/src/OgreBallApplication.cpp
+++ a4/src/OgreBallApplication.cpp
@@ -16,34 +16,69 @@ This source file is part of the
 */
 #include "OgreBallApplication.h"
 
-//-------------------------------------------------------------------------------------
 OgreBallApplication::OgreBallApplication(void)
 {
+  mPhysics = new Physics(btVector3(0, 0, 0));
+  mTimer = OGRE_NEW Ogre::Timer();
+  mTimer->reset();
 }
+
 //-------------------------------------------------------------------------------------
 OgreBallApplication::~OgreBallApplication(void)
 {
 }
 
 //-------------------------------------------------------------------------------------
+void OgreBallApplication::createCamera(void) {
+  BaseApplication::createCamera();
+  mCamera->setPosition(0,0,500);
+}
+
+void OgreBallApplication::createFrameListener(void) {
+  BaseApplication::createFrameListener();
+  
+  // Initialize CEGUI
+  CEGUI::Imageset::setDefaultResourceGroup("Imagesets");                                                         
+  CEGUI::Font::setDefaultResourceGroup("Fonts");                                                                 
+  CEGUI::Scheme::setDefaultResourceGroup("Schemes");                                                             
+  CEGUI::WidgetLookManager::setDefaultResourceGroup("LookNFeel");                                                
+  CEGUI::WindowManager::setDefaultResourceGroup("Layouts");                                                      
+                                                                                                                 
+  mRenderer = &CEGUI::OgreRenderer::bootstrapSystem();                                                           
+  CEGUI::SchemeManager::getSingleton().create("TaharezLook.scheme");                                             
+  CEGUI::SchemeManager::getSingleton().create("WindowsLook.scheme");
+}
+
+//-------------------------------------------------------------------------------------
 void OgreBallApplication::createScene(void)
 {
 	mSceneMgr->setAmbientLight(Ogre::ColourValue(.5f,.5f,.5f));
-	Ogre::Entity* ogreHead = mSceneMgr->createEntity("Head", "sphere.mesh");
-	Ogre::Entity* Ball = mSceneMgr->createEntity("Ball", "sphere.mesh");
-	
-   	Ogre::SceneNode* headNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("HeadNode");
-	headNode->attachObject(ogreHead);
-	headNode->scale(.1,.1,.1);
-	
-	Ogre::SceneNode* ballNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("BallNode");
-	ballNode->attachObject(Ball);
-	ballNode->scale(.1,.1,.1);
+        new OgreBall(mSceneMgr, "ball", "ball", "penguin.mesh", 0, mPhysics);
 
 	Ogre::Light* light = mSceneMgr->createLight("MainLight");
 	light->setPosition(20.0f, 80.0f, 50.0f);
 }
 
+//-------------------------------------------------------------------------------------
+bool OgreBallApplication::frameStarted( const Ogre::FrameEvent &evt ) {
+  return BaseApplication::frameStarted(evt);
+}
+
+//-------------------------------------------------------------------------------------
+bool OgreBallApplication::keyPressed( const OIS::KeyEvent &arg ) {
+  return BaseApplication::keyPressed(arg);
+}
+
+bool OgreBallApplication::keyReleased( const OIS::KeyEvent &arg ) {
+  return BaseApplication::keyReleased(arg);
+}
+
+bool OgreBallApplication::mouseMoved( const OIS::MouseEvent &arg ) {
+  return BaseApplication::mouseMoved(arg);
+}
+bool OgreBallApplication::mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id ) {
+  return BaseApplication::mouseReleased(arg, id);
+}
 
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
--- a4/src/OgreBallApplication.h
+++ a4/src/OgreBallApplication.h
@@ -1,32 +1,55 @@
 /*
------------------------------------------------------------------------------
-Filename:    OgreBallApplication.h
------------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  Filename:    OgreBallApplication.h
+  -----------------------------------------------------------------------------
 
-This source file is part of the
-   ___                 __    __ _ _    _ 
+  This source file is part of the
+  ___                 __    __ _ _    _
   /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
- //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
-/ \_// (_| | | |  __/  \  /\  /| |   <| |
-\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
-      Tutorial Framework
-      http://www.ogre3d.org/tikiwiki/
------------------------------------------------------------------------------
+  //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+  / \_// (_| | | |  __/  \  /\  /| |   <| |
+  \___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+  |___/
+  Tutorial Framework
+  http://www.ogre3d.org/tikiwiki/
+  -----------------------------------------------------------------------------
 */
 #ifndef __OgreBallApplication_h_
 #define __OgreBallApplication_h_
 
+#include <CEGUI/CEGUI.h>
+#include <CEGUI/RendererModules/Ogre/CEGUIOgreRenderer.h>
+
 #include "BaseApplication.h"
+#include "GameObjectDescription.h"
+#include "Physics.h"
 
 class OgreBallApplication : public BaseApplication
 {
-public:
-    OgreBallApplication(void);
-    virtual ~OgreBallApplication(void);
+ public:
+  OgreBallApplication(void);
+  virtual ~OgreBallApplication(void);
 
-protected:
-    virtual void createScene(void);
+  bool frameStarted(const Ogre::FrameEvent &evt);
+
+ protected:
+  virtual void createScene(void);
+  void createCamera(void);
+  void createFrameListener(void);  // GUI Setup
+
+  bool keyPressed( const OIS::KeyEvent &arg );
+  bool keyReleased( const OIS::KeyEvent &arg );
+
+  bool mouseMoved( const OIS::MouseEvent &arg );
+  bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+
+  Ogre::Timer *mTimer;
+  Physics *mPhysics;
+
+  // CEGUI
+  CEGUI::OgreRenderer* mRenderer;
+  CEGUI::WindowManager* Wmgr;
+  CEGUI::Window* menu;
 };
 
 #endif // #ifndef __OgreBallApplication_h_
