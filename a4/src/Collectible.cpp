7b6fd99a524db8903964c62115635f9c92aafd37
Kevin Yeh <kevinyeah@utexas.edu>
2014-04-10 20:25:43 -0500

    remove friction; script organization

--- a4/src/Collectible.cpp
+++ a4/src/Collectible.cpp
@@ -1,12 +1,13 @@
 #include "OgreBallApplication.h"
 #include "GameObject.h"
 #include "GameObjectDescription.h"
+#include "../libs/MeshStrider.h"
 
 Collectible::Collectible(Ogre::SceneManager *mgr, Ogre::String _entName, Ogre::String _meshName, Ogre::String _nodeName, Ogre::SceneNode* parentNode,
-                   Physics* _physics, 
-                   btVector3 origin, btVector3 scale,
-                   btVector3 velocity, btScalar _mass, btScalar _rest,
-                   btVector3 _localInertia, btQuaternion *rotation)
+                         Physics* _physics,
+                         btVector3 origin, btVector3 scale,
+                         btVector3 velocity, btScalar _mass, btScalar _rest,
+                         btVector3 _localInertia, btQuaternion *rotation)
   : GameObject(mgr, _entName, _nodeName, parentNode, _physics, origin, scale, velocity, _mass, _rest, _localInertia, rotation)
 {
 
@@ -15,26 +16,29 @@ Collectible::Collectible(Ogre::SceneManager *mgr, Ogre::String _entName, Ogre::S
 
 
   node->attachObject(entity);
-  node->scale(0.5, 0.5, 0.5);
 
-  Ogre::Vector3 s = entity->getBoundingBox().getHalfSize();
-  collisionShape = new btSphereShape(s[0]);
+  Ogre::MeshPtr meshptr = Ogre::Singleton<Ogre::MeshManager>::getSingletonPtr()->load(_meshName, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+  MeshStrider *strider = new MeshStrider(meshptr.get());
+  collisionShape = new btBvhTriangleMeshShape(strider,true,true);
+
   addToSimulator(Collisions::CollisionTypes::COL_COLLECTIBLE,
                  Collisions::collectibleColliders);
 
   setAmbient(0.5,0.0,0.0);
   setSpecular(0.1,0,0,0.4);
+  if (rotation) rotate(*rotation);
 }
 
 void Collectible::update(float elapsedTime) {
+  GameObject::update(elapsedTime);
   //check collisions
   if(physics->checkCollisions(this)){
     for(int i = 0; i < contexts.size(); i++){
       if(contexts[i]->object){
         OgreBall *ob = dynamic_cast<OgreBall*>(contexts[i]->object);
-        if(ob){          
+        if(ob){
           //TODO: Update score and remove collectible from scene here
-        } 
+        }
       }
     }
   }
