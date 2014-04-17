d014a2e2603cc0e6bc6e206a2838bf46e87b49bc
Alyssa Sallean <krazzle@cs.utexas.edu>
2014-04-17 00:12:22 -0500

    Revert "lul so like camera is fairy close"
    This reverts commit 8509158505f38617b4125fd5c4d0b5dbaccd648e.

--- a4/src/Interpolator.cpp
+++ a4/src/Interpolator.cpp
@@ -5,7 +5,6 @@ btVector3 Interpolator::interpV3(float& currTime, float elapsedTime, float total
   currTime += elapsedTime;
   if (currTime >= totalTime) {
     currTime = totalTime;
-    printf("NOT RETURNING NEW VECTOR\n");
     return second;
   }
 
