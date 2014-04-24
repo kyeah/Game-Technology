9a8d5509b1ace35a0ab011e941512731f394e8ad
Alyssa Sallean <krazzle@cs.utexas.edu>
2014-04-23 23:19:04 -0500

    gates flood open

--- a4/src/GoalObject.cpp
+++ a4/src/GoalObject.cpp
@@ -20,8 +20,19 @@ GoalObject::GoalObject(Ogre::SceneManager *mgr, Ogre::String _entName, Ogre::Str
   : GameObject(mgr, _entName, _nodeName, parentNode, _physics, origin, scale, velocity, _mass, _rest, _localInertia, rotation)
 {
 
+  Ogre::Vector3 nodeWorldPos = node->_getDerivedPosition();
+   
+  leftFlap = mgr->getRootSceneNode()->createChildSceneNode(_entName + "leftFlap");
+  rightFlap = mgr->getRootSceneNode()->createChildSceneNode(_entName + "rightFlap");
+  leftFlap->setPosition(Ogre::Vector3(nodeWorldPos[0] - 190, nodeWorldPos[1] + 60, nodeWorldPos[2]));
+  rightFlap->setPosition(Ogre::Vector3(nodeWorldPos[0] + 190, nodeWorldPos[1] + 60, nodeWorldPos[2]));
+  Ogre::SceneNode* leftChild = leftFlap->createChildSceneNode();
+  Ogre::SceneNode* rightChild = rightFlap->createChildSceneNode();
+
+/*
   leftFlap = node->createChildSceneNode("leftFlap", Ogre::Vector3(-125,60,0));
   rightFlap = node->createChildSceneNode("rightFlap", Ogre::Vector3(125,60,0));
+*/
   Ogre::SceneNode* leftProtector = node->createChildSceneNode("leftProtector", Ogre::Vector3(-250,0,0));
   Ogre::SceneNode* rightProtector = node->createChildSceneNode("rightProtector", Ogre::Vector3(250,0,0));
 
@@ -32,20 +43,24 @@ GoalObject::GoalObject(Ogre::SceneManager *mgr, Ogre::String _entName, Ogre::Str
   Ogre::Entity* leftProtectorEntity = mgr->createEntity(_entName + "leftProtector", "robot.mesh");
   Ogre::Entity* rightProtectorEntity = mgr->createEntity(_entName + "rightProtector", "robot.mesh");
 
-  leftFlap->attachObject(leftFlapEntity);
-  leftFlap->scale(2,1,0.01);
-  rightFlap->attachObject(rightFlapEntity);
-  rightFlap->scale(2,1,0.01);
+  leftChild->attachObject(leftFlapEntity);
+  leftChild->scale(2,1,0.01);
+  leftChild->translate(65,0,0);
+  rightChild->attachObject(rightFlapEntity);
+  rightChild->scale(2,1,0.01);
+  rightChild->translate(-65,0,0);
   leftProtector->attachObject(leftProtectorEntity);
   leftProtector->scale(5,5,.01);
   rightProtector->attachObject(rightProtectorEntity);
   rightProtector->scale(5,5,.01);
   rightProtector->yaw((Ogre::Radian)3.14159);
 
-  Ogre::Vector3 leftFlapEdge = leftFlap->getPosition();
-  axis1 = Ogre::Vector3(leftFlapEdge[1], -leftFlapEdge[0], 0);
+/*  Ogre::Vector3 leftFlapEdge = leftFlap->getPosition();
+  axis1 = Ogre::Vector3(0, 5, 5);
   Ogre::Vector3 rightFlapEdge = rightFlap->getPosition();
-  axis2 = Ogre::Vector3(rightFlapEdge[1], -rightFlapEdge[0], 0);
+  axis2 = Ogre::Vector3(5, 5, 0);
+*/
+
 
   leftFlap->_update(true,true);
   rightFlap->_update(true,true);
@@ -70,20 +85,20 @@ void GoalObject::update(float elapsedTime) {
         Sounds::playSoundEffect(mHitSound.c_str(), (Sounds::MAX_VOLUME / 2));
 	leftFlapEntity->setMaterialName("OgreBall/Passed");
         rightFlapEntity->setMaterialName("OgreBall/Passed");
- 	swinging = 360;
+ 	swinging = 36;
 	OgreBallApplication::getSingleton()->activity->handleGameEnd();
       }
     }
   }
 
-  Ogre::Radian rad = (Ogre::Radian).0872664626;
+  Ogre::Radian rad = (Ogre::Radian).01745328925;
   if(swinging > 0){
-	if(swinging > 180){
-		leftFlap->rotate(axis1, rad);
-		rightFlap->rotate(axis2, -rad);
+	if(swinging > 18){	
+		leftFlap->rotate(Ogre::Quaternion(Ogre::Degree(10), Ogre::Vector3(0,1,0)), Ogre::Node::TransformSpace::TS_WORLD);
+		rightFlap->rotate(Ogre::Quaternion(Ogre::Degree(-10), Ogre::Vector3(0,1,0)), Ogre::Node::TransformSpace::TS_WORLD);
 	}else {
-		leftFlap->rotate(axis1, -rad);
-		rightFlap->rotate(axis2, rad);
+		leftFlap->rotate(Ogre::Quaternion(Ogre::Degree(-10), Ogre::Vector3(0,1,0)), Ogre::Node::TransformSpace::TS_WORLD);
+		rightFlap->rotate(Ogre::Quaternion(Ogre::Degree(10), Ogre::Vector3(0,1,0)), Ogre::Node::TransformSpace::TS_WORLD);
 	}
 	swinging--;
 
